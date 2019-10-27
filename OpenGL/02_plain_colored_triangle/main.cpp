55b28326c00a2470a9052aca35dd4105b988e072
hiperlogic <rodrigod@hiperlogic.com.br>
2019-10-26 23:31:53 -0300

    Document and code

--- OpenGL/02_plain_colored_triangle/main.cpp
+++ OpenGL/02_plain_colored_triangle/main.cpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <cstdlib>
 #include <string>
+#include <vector>
 using namespace std;
 /*
     Includes the Glew (OpenGL Extension Wrangler) in order to verify and use the most recent OpenGL
@@ -23,6 +24,7 @@ using namespace std;
 #include <glm/glm.hpp>
 using namespace glm;
 
+#include <loadShader.hpp>
 
 /*
     WindowAppWrapper implements the basic system to initialize resources, process calls (or idle) 
@@ -77,14 +79,13 @@ class WindowAppWrapper {
                 std::cerr << "GLFW Failed to initialize" <<std::endl;
                 return EXIT_FAILURE;
             }
-
-/*             
+            
             glfwWindowHint(GLFW_SAMPLES, 4);
             glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
             glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
             glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
             glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
- */
+
             this->window = glfwCreateWindow(this->win_width, this->win_height, this->win_title.c_str(), NULL, NULL);
             if( this->window == NULL ){
                 std::cerr << "GLFW Failed to initialize" << std::endl;
@@ -104,49 +105,114 @@ class WindowAppWrapper {
             return EXIT_SUCCESS;
         }
 
+        // Create the shader type, load the source and compile it.
+        GLuint createShader(GLchar* shader_name, GLenum shaderType){
+            GLuint shader = glCreateShader(shaderType);
+            string shaderSource = LoadShader(string(shader_name));
+            const char* str_p = shaderSource.c_str();
+            const char* const* sources = &str_p;
+            glShaderSource(shader, 1, sources, nullptr);
+            glCompileShader(shader);
+            return shader;
+        }
+
+/*
+        GLuint createShaders(GLchar** shader_names, GLuint numFiles, GLenum shaderType){
+            GLuint shader = glCreateShader(shaderType);
+            vector <string> shaderSources;
+            for(int i=0; i<numFiles; i++){
+                string shaderSource = LoadShader(string(shader_names[0]));
+
+            }
+            glShaderSource(shader, 1, &shaderSource.c_str, nullptr);
+            glCompileShader(shader);
+            return shader;
+        }
+*/
+
         void mainLoop(){
             glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);   
 
-            // Tell OpenGL state machine which matrix we will configure
-            glMatrixMode(GL_PROJECTION);
-            // Set the matrix to the identity
-            glLoadIdentity();
-            // Configure the matrix acording to the orthographic view
-            // Homogeneous coordinates: Horizontal, Vertical and Depth (X,Y,Z) from -1.0 to 1.0
-            glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0); 
-            // Tell OpenGL we are no longer modifying the projection matrix, but the modelview matrix.
-            glMatrixMode(GL_MODELVIEW);
-            glLoadIdentity();
-            // Configure the clear color as we already have learned
-            glClearColor(0.0, 0.0, 0.0, 0.0);
+            // The triangle vertices to be drawn. They are placed in a contiguous memory area
+            GLfloat triangle_buffer_data[]={
+                -1.0f, -1.0f, 0.0f,
+                1.0f, -1.0f, 0.0f,
+                0.0f, 1.0f, 0.0f
+            };
 
-            glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-            float colors[] = {1.0f, 0.0f, 0.0f, 1.0f, 0.1f, 0.8f, 1.0f, 1.0f, 0.0f}; //Red, somewhat violet and yellow
-            int corrCol = 1; // The somewhat violet is the color of the previous code
-            glColor3f(colors[corrCol*3], colors[corrCol*3+1],colors[corrCol*3+2]);
+            GLuint vertexShader = createShader("vertex1.glsl", GL_VERTEX_SHADER);
+            GLuint fragmentShader = createShader("pixel1.glsl", GL_FRAGMENT_SHADER);
+
+            GLuint shaderProgram = glCreateProgram();
+            glAttachShader(shaderProgram, vertexShader);
+            glAttachShader(shaderProgram, fragmentShader);
+
+            glLinkProgram(shaderProgram);
+            glUseProgram(shaderProgram);
+
+            glDetachShader(shaderProgram, vertexShader);
+            glDetachShader(shaderProgram, fragmentShader);
+
+            glDeleteShader(vertexShader);
+            glDeleteShader(fragmentShader);
+
+            // Stores the identifier of the vertex array in the OpenGL state machine
+            GLuint vertexArrayID;
+            // Generates and stores the vertex array index (or name)
+            glGenVertexArrays(1, &vertexArrayID);
+            // Sets the generated vertex array as the current in order to receive the data
+            glBindVertexArray(vertexArrayID);
+
+            // Store the vertex buffer identifier
+            GLuint vertexbuffer;
+
+            // Generate 1 buffer and store its identifier in the proper id
+            glGenBuffers(1, &vertexbuffer);
+
+            // Bind the current buffer to the buffer generated whose ID is stored in vertexbuffer
+            // This buffer is an array buffer
+            glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+            // Set the current buffer data.
+            // The data is provided as an array buffer
+            // The size of the data (buffer) in memory
+            // The memory address where the buffer starts
+            // The data is static (not streamed nor dynamic. It does not change) and to be drawn (not to be read or copy)
+            cout<<"Size: "<<sizeof(triangle_buffer_data)<<"divided by floar: "<<sizeof(triangle_buffer_data)/sizeof(GLfloat);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_buffer_data), triangle_buffer_data, GL_STATIC_DRAW);
+
+            // Configure the clear color as we already have learned
+            glClearColor(0.0, 0.0, 0.5, 0.0);
 
             do{
                 // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
                 glClear( GL_COLOR_BUFFER_BIT );
+
                 // Draw a White (current color) triangle!
+                //Enable the vertex attribute array of index 0
+                glEnableVertexAttribArray(0);
+                // Bind the current buffer to the vertex buffer
+                glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+                // Set the buffer attributions
+                glVertexAttribPointer(
+                    0,          // Whith vertex attrib array index to use
+                    3,          // How many data does the buffer holds
+                    GL_FLOAT,    // What kind of data the buffer holds
+                    GL_FALSE,    //  Is the data normalized?
+                    0,           // Do not consider stride
+                    (void*)0    // No offset to be used
+                );
+                // Send the command to draw the data from the configured buffers
+                // Consider the triangle as the primitive to process
+                // Starts with the data at position 0
+                // Consider 3 data to use
+                glDrawArrays(GL_TRIANGLES, 0, 3);
 
-                glColor3f(colors[corrCol*3], colors[corrCol*3+1],colors[corrCol*3+2]);
-
-                glBegin(GL_TRIANGLES);
-                    glVertex3f(-1.0,-1.0,0.0);
-                    glVertex3f(1.0,-1.0,0.0);
-                    float top[3] = {0.0,1.0,0.0};
-                    glVertex3fv(top);
-                glEnd();
-
-                glFlush();
+                // The OpenGL resource for the vertex attrib array with index 0 has already been processed
+                // It can be deactivated for future uses.
+                glDisableVertexAttribArray(0);
                 // Swap buffers
                 glfwSwapBuffers(window);
                 glfwPollEvents();
-
-                if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS){
-                    corrCol = (corrCol+1)%3;
-                }
 
             } // Check if the ESC key was pressed or the window was closed
             while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
