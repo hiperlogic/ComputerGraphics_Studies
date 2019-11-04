76cd1268882455bc2dd6b6ad1c0787e7c89f08ab
hiperlogic <rodrigod@hiperlogic.com.br>
2019-11-04 13:53:00 -0300

    project num greater than branch eq branch

---
+++ OpenGL/15_specular_lighting/main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+#include <string>
+using namespace std;
+/*
+    Includes the Glew (OpenGL Extension Wrangler) in order to verify and use the most recent OpenGL
+    implementations. This must be included prior to any library that uses OpenGL.
+*/
+#include <GL/glew.h>
+
+/*
+    Includes the GLFW to manage the windows calls, surface rendering and events independent of
+    operating system.
+*/
+#include <GLFW/glfw3.h>
+
+/*
+    Includes the routines for matrices manipulation. These are essential for computer graphics
+    operations.
+*/
+#include <glm/glm.hpp>
+using namespace glm;
+
+
+/*
+    WindowAppWrapper implements the basic system to initialize resources, process calls (or idle) 
+    and perform the cleanup of the resources.
+    This class will be improved within the course of the tutorials.
+    Improvements Added:
+        Class created
+*/
+class WindowAppWrapper {
+    public:
+
+        WindowAppWrapper(){
+            this->is_running = 0;
+        }
+
+        ~WindowAppWrapper(){
+        }
+
+        void run(){
+            this->is_running = 1;
+            int result = initSystem();
+            if( result != EXIT_FAILURE )
+                mainLoop();
+            cleanup();
+            this->is_running = 0;
+        }
+
+        void set_height(int new_height){
+            if(this->is_running) return;
+            this->win_height = new_height;
+        }
+
+        void set_width(int new_width){
+            if(this->is_running) return;
+            this->win_width = new_width;
+        }
+
+    private:
+        GLFWwindow* window;
+        int win_width = 1024;
+        int win_height = 768;
+        string win_title = "First OpenGL Window\n";
+        bool is_running;
+
+        static void error_callback(int error, const char* description){
+            fprintf(stderr, "Error: %s\n", description);
+        }
+
+        int initSystem(){
+            glfwSetErrorCallback( WindowAppWrapper::error_callback );
+            if(!glfwInit()){
+                std::cerr << "GLFW Failed to initialize" <<std::endl;
+                return EXIT_FAILURE;
+            }
+
+            glfwWindowHint(GLFW_SAMPLES, 4);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+            this->window = glfwCreateWindow(this->win_width, this->win_height, this->win_title.c_str(), NULL, NULL);
+            if( this->window == NULL ){
+                std::cerr << "GLFW Failed to initialize" << std::endl;
+                glfwTerminate();
+                return EXIT_FAILURE;
+            }
+
+            glfwMakeContextCurrent(this->window);
+            glewExperimental = true;
+
+            if (glewInit() != GLEW_OK) {
+                std::cerr << "GLEW Failed to initialize" <<std::endl;
+                glfwTerminate();
+                return EXIT_FAILURE;
+            }
+
+            return EXIT_SUCCESS;
+        }
+
+        void mainLoop(){
+            glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);   
+
+            glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+
+            do{
+                // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+                glClear( GL_COLOR_BUFFER_BIT );
+
+                // Draw nothing, see you in tutorial 2 !
+
+                // Swap buffers
+                glfwSwapBuffers(window);
+                glfwPollEvents();
+
+            } // Check if the ESC key was pressed or the window was closed
+            while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+                glfwWindowShouldClose(window) == 0 );
+        }
+
+        void cleanup(){
+
+        }
+};
+
+int main() {
+    WindowAppWrapper *app = new WindowAppWrapper();
+
+    try {
+        app->run();
+    } catch (const std::exception& e){
+        std::cerr <<e.what() <<std::endl;
+        delete app;
+        return EXIT_FAILURE;
+    }
+
+    delete app;
+    return EXIT_SUCCESS;
+}
