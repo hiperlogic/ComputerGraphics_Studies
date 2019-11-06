8f8f74e68dc34e8fbe4adc525b6a5e255d58572d
hiperlogic <rodrigod@hiperlogic.com.br>
2019-11-06 13:39:26 -0300

    Select suitable format

--- Vulkan/01_plain_triangle/main.cpp
+++ Vulkan/01_plain_triangle/main.cpp
@@ -548,6 +548,15 @@ class WindowAppWrapper {
         return indices;
     }
 
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+        for(const auto& availableFormat : availableFormats) {
+            if (availableFormat.format == VK_FORMAT_R8G8B8A8_UNORM && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR){
+                return availableFormat;
+            }
+        }
+        return availableFormat[0];
+    }
+
 };
 
 int main() {
