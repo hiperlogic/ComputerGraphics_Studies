8875172fe032c6184bf35eac341dbd016725a046
hiperlogic <rodrigod@hiperlogic.com.br>
2019-11-07 21:53:09 -0300

    Forgot to call create surface

--- Vulkan/01_plain_triangle/main.cpp
+++ Vulkan/01_plain_triangle/main.cpp
@@ -247,10 +247,13 @@ class WindowAppWrapper {
             }
             std::cout<<"Starting the Debugger"<<std::endl;
             setupDebugMessenger();
+            createSurface();
             setPhysicalDevice();
             createLogicalDevice();
             createSwapChain();
             createImageViews();
+            createRenderPass();
+            createGraphicsPipeline();
             return result;
         }
 
@@ -594,7 +597,7 @@ class WindowAppWrapper {
 
     VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
         for(const auto& availableFormat : availableFormats) {
-            if (availableFormat.format == VK_FORMAT_R8G8B8A8_UNORM && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR){
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR){
                 return availableFormat;
             }
         }
